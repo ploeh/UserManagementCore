50b6d5c7d9341c6e0d0a01435786794ed751c0e8
Mark Seemann <mark@ploeh.dk>
2019-07-31 16:32:23 +0200

    Add build scripts

---
+++ CSharp/Build.fsx
@@ -0,0 +1,15 @@
+﻿#r "paket:
+nuget Fake.DotNet.Cli
+nuget Fake.Core.Target //"
+#load "./.fake/build.fsx/intellisense.fsx"
+
+open Fake.DotNet
+open Fake.Core
+
+// Targets
+Target.create "Build" <| fun _ ->
+    "UserManagement.sln"
+    |> DotNet.build id
+
+// start build
+Target.runOrDefault "Build"
---
+++ CSharp/build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+fake run Build.fsx
